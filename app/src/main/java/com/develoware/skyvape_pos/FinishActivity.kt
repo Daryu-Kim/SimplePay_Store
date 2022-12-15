428295260c5409e64844c75607fd08b577df9200
junwon1234 <najjang1105@naver.com>
2022-12-15 16:52:17 +0900

    2022-12-15 16:52 Code Refactoring & Function Update

--- app/src/main/java/com/develoware/skyvape_pos/FinishActivity.kt
+++ app/src/main/java/com/develoware/skyvape_pos/FinishActivity.kt
@@ -1,9 +1,11 @@
 package com.develoware.skyvape_pos
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.develoware.skyvape_pos.databinding.ActivityFinishBinding
 import com.develoware.skyvape_pos.databinding.ActivityPaymentBinding
@@ -11,6 +13,8 @@ import com.develoware.skyvape_pos.databinding.ActivitySplashBinding
 import com.develoware.skyvape_pos.databinding.ActivityTransferBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class FinishActivity : AppCompatActivity() {
     private var mBinding: ActivityFinishBinding? = null
@@ -23,18 +27,35 @@ class FinishActivity : AppCompatActivity() {
 
     val db = Firebase.firestore
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityFinishBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val current_timestamp = LocalDateTime.now()
+        val format_timestamp = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val receipt_timestamp = current_timestamp.format(format_timestamp)
+        val receipt_data = hashMapOf(
+            "timestamp" to receipt_timestamp
+        )
+
+        db.collection("Eunhaeng_Receipt")
+            .document(receipt_timestamp)
+            .set(receipt_data)
+        // basket_data에 있는 데이터를 Firestore의 Eunhaeng_Receipt -> 날짜 document에 넣어주기.
+
         Handler().postDelayed({
-            basketReset("Eunhaeng")
             val intent = Intent(this, PosActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
         },DURATION)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        basketReset("Eunhaeng")
     }
 
     companion object {
